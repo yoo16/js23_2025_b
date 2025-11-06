0765873ee8162d23349c5d4438ae195848001d51
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-06 11:10:49 +0900

    update

--- 04_npm/load_env.js
+++ 04_npm/load_env.js
@@ -5,9 +5,9 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 // TODO: 環境変数の取得
-const HOST = "";
-const PORT = "";
-const SESSION_SECRET = "";
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 // 結果表示
 let result = { HOST, PORT, SESSION_SECRET };

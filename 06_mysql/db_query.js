fa6d0587285ac0dc29c5ed0ab724a645194adae4
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-27 11:28:05 +0900

    update

--- 06_mysql/db_query.js
+++ 06_mysql/db_query.js
@@ -1,10 +1,11 @@
+// lib/db.js から pool をインポート（DB接続済みのはず）
 import { pool } from './lib/db.js';
 
 // TODO: SQL: users テーブルから 5 件取得
-const sql = ""
+const sql = "SELECT * FROM users LIMIT 5;"
 
-// TODO: SQL 実行
-const [rows] = [];
+// TODO: SQLクエリー実行:非同期通信
+const [rows] = await pool.query(sql)
 
 // 表示
 console.table(rows);
